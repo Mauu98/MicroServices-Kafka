a67d3bac6ad73579097657a3a52319559ee79430
Mauricio Pino <mauricio.pino@ross.com.ar>
2023-11-24 18:29:28 -0300

    - Tests added

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -1,5 +1,7 @@
 package com.food.ordering.system.order.service.domain;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.order.service.domain.entity.Customer;
@@ -57,26 +59,35 @@ public class OrderCreateCommandHandler {
         Order orderResult = saveOrder(order);
         log.info("Order is created with id: {}", orderResult.getId().getValue());
         applicationDomainEventPublisher.publish(orderCreatedEvent);
-        return orderDataMapper.orderToCreateOrderResponse(orderResult);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(orderResult);
+            log.warn("Order Result: " + json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return orderDataMapper.orderToCreateOrderResponse(orderResult, "Order created successfully!");
     }
 
     private Restaurant checkRestaurant(CreateOrderCommand createOrderCommand) {
-          Restaurant restaurant = orderDataMapper.createOrderCommandToRestaurant(createOrderCommand);
-          Optional<Restaurant> optionalRestaurant = restaurantRepository.findRestaurantInformation(restaurant);
-          if (optionalRestaurant.isEmpty()) {
-              log.warn("Could not find restaurant with restaurant id: {}", createOrderCommand.getRestaurantId());
-              throw new OrderDomainException("Could not find restaurant with restaurant id: " +
-                      createOrderCommand.getRestaurantId());
-          }
-          return optionalRestaurant.get();
+        Restaurant restaurant = orderDataMapper.createOrderCommandToRestaurant(createOrderCommand);
+        Optional<Restaurant> optionalRestaurant = restaurantRepository.findRestaurantInformation(restaurant);
+        if (optionalRestaurant.isEmpty()) {
+            log.warn("Could not find restaurant with restaurant id: {}", createOrderCommand.getRestaurantId());
+            throw new OrderDomainException("Could not find restaurant with restaurant id: " +
+                    createOrderCommand.getRestaurantId());
+        }
+        return optionalRestaurant.get();
     }
 
     private void checkCustomer(UUID customerId) {
-       Optional<Customer> customer = customerRepository.findCustomer(customerId);
-       if (customer.isEmpty()) {
-           log.warn("Could not find customer with customer id: {}", customerId);
-           throw new OrderDomainException("Could not find customer with customer id: " + customer);
-       }
+        Optional<Customer> customer = customerRepository.findCustomer(customerId);
+        if (customer.isEmpty()) {
+            log.warn("Could not find customer with customer id: {}", customerId);
+            throw new OrderDomainException("Could not find customer with customer id: " + customer);
+        }
     }
 
     private Order saveOrder(Order order) {
